86c812b014421d158978c3863f6a1913052bde6c
Tim Khasanov <irugasan@gmail.com>
2026-08-26 12:02:01 +0700

    Revert "feat(dependency): add policies support templatefile() (#1)"
    This reverts commit a05287d0e107026431feb8ce19ede986bf271d61.

--- main.tf
+++ main.tf
@@ -1,29 +1,8 @@
-locals {
-  policy_files = {
-    for f in fileset(var.policies_dir, "**/*.hcl") :
-    replace(trimsuffix(f, ".hcl"), "/", "-") => f
-  }
-
-  policies = {
-    for name, f in local.policy_files :
-    name => templatefile("${var.policies_dir}/${f}", lookup(var.template_vars, name, {}))
-  }
-
-  orphaned_template_vars = setsubtract(keys(var.template_vars), keys(local.policy_files))
-}
-
-check "template_vars_match_policies" {
-  assert {
-    condition = length(local.orphaned_template_vars) == 0
-    error_message = format(
-      "template_vars has entries for policies that do not exist: %s. Keys are policy names derived from the file path, so policies/team/dev.hcl is keyed as \"team-dev\".",
-      join(", ", local.orphaned_template_vars)
-    )
-  }
-}
-
 resource "vault_policy" "this" {
-  for_each = local.policies
+  for_each = {
+    for f in fileset(var.policies_dir, "**/*.hcl") :
+    replace(trimsuffix(f, ".hcl"), "/", "-") => file("${var.policies_dir}/${f}")
+  }
 
   name   = each.key
   policy = each.value
